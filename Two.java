b77400b02c003b1f6111fb7e959e08a7789e37c5
Nithishs56 <159990028+Nithishs56@users.noreply.github.com>
2026-01-19 18:56:48 +0530

    Add files via upload

---
+++ Two.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class Two{
+    public static void main(String[] args){
+        int a[]={1,4,9};
+        for(int i=a.length-1;i>=0;i--){
+            System.out.print(a[i]);
+        }
+    }
+}
